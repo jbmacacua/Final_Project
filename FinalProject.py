8006e4ff11b53f1a6aa81914932399933a0a9a55
Jonas Brian R. Macacua <jbrmacacua03@gmail.com>
2023-02-16 16:30:52 +0800

    make alexa talk

--- FinalProject.py
+++ FinalProject.py
@@ -11,3 +11,7 @@ listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
