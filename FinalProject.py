c67df8bee7c90a9d4946971e24a5b8d06f73ffd6
Jonas Brian R. Macacua <jbrmacacua03@gmail.com>
2023-02-16 16:35:23 +0800

    Add other commands

--- FinalProject.py
+++ FinalProject.py
@@ -29,3 +29,28 @@ def take_command():
     except:
         pass
     return command
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'who the heck is' in command:
+        person = command.replace('who the heck is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'date' in command:
+        talk('sorry, I have a headache')
+    elif 'are you single' in command:
+        talk('I am in a relationship with wifi')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    else:
+        talk('Please say the command again.')
+
